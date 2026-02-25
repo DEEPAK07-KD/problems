b2559eb9ef559f136f8447a2d2af0286959f8d5c
DEEPAK07-KD <ffdeepak0007@gmail.com>
2026-02-25 08:49:13 +0530

    Day 5

---
+++ Day 5 HW/Mobile.java
@@ -0,0 +1,16 @@
+class Mobile
+{
+	public static void main(String[] args)
+	{
+		String mobileName = "iPhone 15 pro";
+		String mobileBrand = "Apple";
+		float screenSize = 6.1f;
+		int battaryCapacity = 3274;
+		double price = 134999.99;
+		System.out.println(mobileName);
+		System.out.println(mobileBrand);
+		System.out.println(screenSize);
+		System.out.println(battaryCapacity);
+		System.out.println(price);
+	}
+}
