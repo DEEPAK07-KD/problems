ad7e21d9af65c5340e1f39c8a735eb7c7c0da942
DEEPAK07-KD <ffdeepak0007@gmail.com>
2026-02-27 08:17:52 +0530

    Day 7

---
+++ Day 7 HW/ArithmeticOperation.java
@@ -0,0 +1,16 @@
+class ArithmeticOperation
+{
+	public static void main(String args[])
+	{
+		int a=45;
+		int b=55;
+		int z=a+b;
+		System.out.println(z);
+		int c=z-50;
+		System.out.println(c);
+		int d=z*5;
+		System.out.println(d);
+		int e=c/10;
+		System.out.println(e);
+	}
+}
