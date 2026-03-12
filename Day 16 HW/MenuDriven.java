fa01d670335b905afd52fc8ed065bd732ff1cab1
DEEPAK07-KD <ffdeepak0007@gmail.com>
2026-03-12 11:36:13 +0530

    Day 16

---
+++ Day 16 HW/MenuDriven.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class MenuDriven
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.print("INSERT ELEMENTS :");
+		int element=sc.nextInt();
+		int a[]=new int[element];
+		int ecount=0;
+		int ocount=0;
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print("ENTER THE "+i+" INDEX VALUE : ");
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]%2==0)
+			{
+				System.out.println("The Even Numbers is :"+a[i]);
+				ecount++;
+			}
+			else
+			{
+				System.out.println("The Odd Numbers is :"+a[i]);
+				ocount++;
+			}
+		}
+		System.out.println("The Even Count Numbers is :"+ecount);
+		System.out.println("The Odd Count Numbers is :"+ocount);
+		
+	}
+}
---
+++ Day 16 HW/Sum.java
@@ -0,0 +1,12 @@
+class Sum
+{
+	public static void main(String[] args)
+	{
+		int a[]=new int[3];
+		a[0]=4;
+		a[1]=6;
+		a[2]=10;
+			System.out.println("AFTER THE ADDITION :"+(a[0]+a[1]));
+			System.out.println(a[2]);
+	}
+}
